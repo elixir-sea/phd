9f62635bca7c2bd63f5ae6f98276ac3d3b7c8d6c
Donald Plummer <dplummer@avvo.com>
2017-08-16 19:50:38 -0700

    Initial commit

---
+++ lib/phd.ex
@@ -0,0 +1,18 @@
+defmodule Phd do
+  @moduledoc """
+  Documentation for Phd.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Phd.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/phd_test.exs
@@ -0,0 +1,8 @@
+defmodule PhdTest do
+  use ExUnit.Case
+  doctest Phd
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
